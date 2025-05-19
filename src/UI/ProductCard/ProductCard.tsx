2b0b31e790f23a7c533c081b892227a600d2e7e9
DaaNMSR <denispresent4@gmail.com>
2025-05-19 23:13:55 +0300

    create product card

---
+++ src/UI/ProductCard/ProductCard.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../Button';
+import styles from './ProductCard.module.css';
+import basketIcon from './images/basket-icon.svg';
+
+interface ProductCardProps {
+  product: {
+    imagePath: string;
+    title: string;
+    price: string;
+  };
+}
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <div className={styles.productCard}>
+      <img src={product.imagePath} alt={product.title} />
+      <div className={styles.сontent}>
+        <h4 className={styles.title}>{product.title}</h4>
+        <p className={styles.price}>{product.price}</p>
+        <Button iconLeft={basketIcon} size="md" variant="dark" className={styles.buttonTop}>
+          В корзину
+        </Button>
+        <Button size="md" className={styles.buttonBottom}>
+          Купить в кредит
+        </Button>
+      </div>
+    </div>
+  );
+};
