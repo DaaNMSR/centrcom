1a83cac3d80f705c5127676fa2fda5cb6c1e4984
DaaNMSR <denispresent4@gmail.com>
2025-07-04 15:04:48 +0300

    update path for static

--- mock-server/index.ts
+++ mock-server/index.ts
@@ -30,7 +30,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, './public')));
+app.use('/', express.static(path.join(__dirname, './public')));
 app.use('/main-categories', mainCategoriesRouter);
 app.use('/category', categoryProductsRouter);
 app.use('/new-products', productRouter);
